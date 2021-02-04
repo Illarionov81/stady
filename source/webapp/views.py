ecba71f05997ea4eb549d3fde8c215ee40b1cc7f
alex <nuvorish81@mail.ru>
2021-02-04 18:25:12 +0600

    Простая форма

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
+from webapp.forms import ArticleForm
 from webapp.models import Article
 
 
@@ -17,65 +18,42 @@ def article_view(request, pk):
     return render(request, 'article.html', context={'article': article})
 
 
-def article_create_view(request):
+def article_create_view(request, *args, **kwargs):
     if request.method == 'GET':
-        return render(request, 'article_create.html')
+        form = ArticleForm()
+        return render(request, 'article_create.html', context={'form': form})
     elif request.method == 'POST':
-        title = request.POST.get('title')
-        text = request.POST.get('text')
-        author = request.POST.get('author')
-        errors = {}
-        if not title:
-            errors['title'] = 'Title should not be empty!'
-        elif len(title) > 200:
-            errors['title'] = 'Title should be 200 symbols or less!'
-        if not author:
-            errors['author'] = 'Author should not be empty!'
-        elif len(author) > 40:
-            errors['author'] = 'Author should be 40 symbols or less!'
-        if not text:
-            errors['text'] = 'Text should not be empty!'
-        elif len(text) > 3000:
-            errors['text'] = 'Text should be 3000 symbols or less!'
-
-        if len(errors) > 0:
-            article = Article(title=title, author=author, text=text)
-            return render(request, 'article_create.html',
-                          context={'errors': errors, 'article': article})
-        else:
-            article = Article.objects.create(title=title, text=text, author=author)
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article = Article.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text']
+            )
             return redirect('article', pk=article.pk)
+        else:
+            return render(request, 'article_create.html', context={'form': form})
 
 
 def article_update_view(request, pk):
     article = get_object_or_404(Article, pk=pk)
     if request.method == 'GET':
-        return render(request, 'update.html', context={'article': article})
+        form = ArticleForm(initial={
+            'title': article.title,
+            'text': article.text,
+            'author': article.author
+        })
+        return render(request, 'update.html', context={'form': form, 'article': article})
     elif request.method == 'POST':
-        article.title = request.POST.get('title')
-        article.author = request.POST.get('author')
-        article.text = request.POST.get('text')
-        errors = {}
-        if not article.title:
-            errors['title'] = 'Title should not be empty!'
-        elif len(article.title) > 200:
-            errors['title'] = 'Title should be 200 symbols or less!'
-        if not article.author:
-            errors['author'] = 'Author should not be empty!'
-        elif len(article.author) > 40:
-            errors['author'] = 'Author should be 40 symbols or less!'
-        if not article.text:
-            errors['text'] = 'Text should not be empty!'
-        elif len(article.text) > 3000:
-            errors['text'] = 'Text should be 3000 symbols or less!'
-        if len(errors) > 0:
-            return render(request, 'update.html', context={
-                'errors': errors,
-                'article': article
-            })
-        else:
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article.title = form.cleaned_data['title']
+            article.text = form.cleaned_data['text']
+            article.author = form.cleaned_data['author']
             article.save()
             return redirect('article', pk=article.pk)
+        else:
+            return render(request, 'update.html', context={'form': form, 'article': article})
 
 
 def article_delete_view(request, pk):
