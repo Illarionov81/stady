08fbf1bd7fd83655d473b18a3163ba40aa1675bb
alex <nuvorish81@mail.ru>
2021-02-17 21:07:24 +0600

    Аватарка выводится в админке

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 from main.settings import AUTH_USER_MODEL
@@ -7,8 +8,9 @@ from main.settings import AUTH_USER_MODEL
 class MyUserInline(admin.ModelAdmin):
     model = AUTH_USER_MODEL
     exclude = []
-    # list_display = ['birth_date', 'pk']
+    list_display = ['image_img', "username", 'birth_date', 'pk']
+    readonly_fields = ['image_img', ]
+    list_display_links = ['username']
 
 
 admin.site.register(User, MyUserInline)
-# admin.site.register(User, UserAdmin)
--- source/accounts/models.py
+++ source/accounts/models.py
@@ -10,6 +10,16 @@ class User(AbstractUser):
     # def __str__(self):
     #     return '%s' % self.pk
 
+    def image_img(self):
+        if self.avatar:
+            from django.utils.safestring import mark_safe
+            return mark_safe(u'<a href="{0}" target="_blank"><img src="{0}" width="100"/></a>'.format(self.avatar.url))
+        else:
+            return '(Нет изображения)'
+
+    image_img.short_description = 'Картинка'
+    image_img.allow_tags = True
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
