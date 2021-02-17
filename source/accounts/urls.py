fd39ecdc99c54f674b901640fea48ed4b26875d0
alex <nuvorish81@mail.ru>
2021-02-17 17:49:48 +0600

    Регистрация пользователя переделанна на CreateView

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 # from accounts.views import login_view, logout_view
-from accounts.views import register_view
+from accounts.views import RegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('sign-up/', register_view, name='sign_up')
+    path('sign-up/', RegisterView.as_view(), name='sign_up')
 ]
