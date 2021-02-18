5a0b3c2b016b90dda6cc69a028d8fcdc234ae98c
alex <nuvorish81@mail.ru>
2021-02-18 17:33:45 +0600

    Смена пароля сделана на самописной форме-вьюшке

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 # from accounts.views import login_view, logout_view
-from accounts.views import RegisterView, UserDetailView, UserChangeView, PasswordView
+from accounts.views import RegisterView, UserDetailView, UserChangeView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -12,5 +12,5 @@ urlpatterns = [
     path('sign-up/', RegisterView.as_view(), name='sign_up'),
     path('<int:pk>/', UserDetailView.as_view(), name='detail'),
     path('<int:pk>/update/', UserChangeView.as_view(), name='change'),
-    path('password_change/', PasswordView.as_view(), name='password_change'),
+    path('<int:pk>/password_change/', UserPasswordChangeView.as_view(), name='password_change'),
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,13 +1,11 @@
-from django.contrib.auth import authenticate, login, logout, get_user_model
-from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth import login,  get_user_model
 from django.core.paginator import Paginator
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import CreateView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from accounts.forms import MyUserCreationForm, UserChangeForm
+from accounts.forms import MyUserCreationForm, UserChangeForm, PasswordChangeForm
 from accounts.models import User
 
 
@@ -58,9 +56,12 @@ class UserChangeView(UpdateView):
         return reverse('accounts:detail', kwargs={'pk': self.object.pk})
 
 
-class PasswordView(PasswordChangeView):
+class UserPasswordChangeView(UpdateView):
+    model = get_user_model()
     template_name = 'password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
 
     def get_success_url(self):
-        return reverse('accounts:detail', kwargs={'pk': self.request.user.pk})
+        return reverse('accounts:login')
 
