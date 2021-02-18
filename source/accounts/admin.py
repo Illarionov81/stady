a2b7c40a489895e9b7e815e0af57850b2ea9fdd0
alex <nuvorish81@mail.ru>
2021-02-18 17:13:14 +0600

    Переопределил поля UserAdmin для вывода доп полей с модели расширения юзера

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -5,12 +5,28 @@ from .models import User
 from main.settings import AUTH_USER_MODEL
 
 
-class MyUserInline(admin.ModelAdmin):
-    model = AUTH_USER_MODEL
+def _(param):
+    pass
+
+
+class MyUserInline(UserAdmin):
+    # model = get_user_model()
     exclude = []
-    list_display = ['image_img', "username", 'birth_date', 'pk']
+    list_display = ['image_img', "username", 'birth_date', ]
     readonly_fields = ['image_img', ]
     list_display_links = ['username']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined', 'image_img', 'birth_date',)}),
+    )
+
+
+# class ProfileAdmin(UserAdmin):
+#     inlines = [MyUserInline]
 
 
 admin.site.register(User, MyUserInline)
