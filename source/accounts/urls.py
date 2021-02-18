ddca635a0797309e576d2d7ccf12968fff5564e0
alex <nuvorish81@mail.ru>
2021-02-18 10:02:40 +0600

    Сделана страничка пользователя

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 # from accounts.views import login_view, logout_view
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserDetailView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('sign-up/', RegisterView.as_view(), name='sign_up')
+    path('sign-up/', RegisterView.as_view(), name='sign_up'),
+    path('<int:pk>/', UserDetailView.as_view(), name='detail')
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from accounts.forms import MyUserCreationForm
 from accounts.models import User
@@ -24,3 +26,21 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('webapp:index')
         return next_url
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        articles = self.object.articles.order_by('-created_at')
+        paginator = Paginator(articles, self.paginate_related_by, orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['articles'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
