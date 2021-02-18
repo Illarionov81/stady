f5f96de70d46a3332d9a892cff7823704dac5614
alex <nuvorish81@mail.ru>
2021-02-19 00:27:17 +0600

    Пример создания статьи через api на джанговсой вьюшке

--- source/api_1/urls.py
+++ source/api_1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from api_1.views import json_echo_view
+from api_1.views import json_echo_view, get_token_view, ArticleCreateView
 
 app_name = 'api_1'
 
 urlpatterns = [
-    path('echo/', json_echo_view)
+    path('echo/', json_echo_view),
+    path('get-token/', get_token_view),
+    path('article-create/', ArticleCreateView.as_view(), name='article_create')
 ]
--- source/api_1/views.py
+++ source/api_1/views.py
@@ -2,17 +2,47 @@ from django.shortcuts import render
 
 import json
 from datetime import datetime
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
+
+from webapp.models import Article
 
 
 @ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    else:
+        return HttpResponseNotAllowed("Only GET response are allowed")
+
+
 def json_echo_view(request, *args, **kwargs):
     answer = {
         'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-        'method': request.method,
+        'method': request.method
     }
-    answer_as_json = json.dumps(answer)
-    response = HttpResponse(answer_as_json)
-    response['Content-Type'] = 'application/json'
-    return response
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        answer['data'] = data
+    return JsonResponse(answer)
+
+
+class ArticleCreateView(View):
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        print(data)
+        article = Article.objects.create(**data)
+        # article = Article.objects.create(
+        #     author_id=data['author_id'],
+        #     title=data['title'],
+        #     text=data['text']
+        # )
+        return JsonResponse({
+            'pk': article.pk,
+            'author_id': article.author_id,
+            'title': article.title,
+            'text': article.text,
+            'created_at': article.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at': article.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+        })
